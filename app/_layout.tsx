95b55e2529d4fc0196045fbe119ec9a57241c379
yanquielarango <yanquiel.arango@gmail.com>
2025-01-02 22:55:57 +0100

    creating a Medi-Tracker app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,32 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {tokenCache} from "@/utils/cache";
+import {ClerkLoaded, ClerkProvider} from "@clerk/clerk-expo";
+import {Colors} from "@/constants/Colors";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+    const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+    if (!publishableKey) {
+        throw new Error('Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env')
+    }
+
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+      "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+      "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+      "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+      "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+      "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+      "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
   });
 
   useEffect(() => {
@@ -28,12 +40,18 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+   <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+       <ClerkLoaded>
+           <Stack screenOptions={{contentStyle: {backgroundColor: Colors.background}}}>
+               <Stack.Screen name="index" options={{headerShown: false}} />
+               {/*<Stack.Screen name="(auth)" options={{ headerShown: false }} />*/}
+               <Stack.Screen name="+not-found" />
+           </Stack>
+           <StatusBar style="dark" />
+       </ClerkLoaded>
+
+   </ClerkProvider>
+
+
   );
 }
